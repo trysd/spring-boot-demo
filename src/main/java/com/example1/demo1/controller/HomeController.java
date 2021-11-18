d18e34780ebe583c235740e379fa3e337602256a
trysd <itrysd@icloud.com>
2021-11-18 15:13:04 +0900

    first commit

---
+++ src/main/java/com/example1/demo1/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example1.demo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @RequestMapping("/")
+  public String home(){
+    return "Hello World!";
+  }
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
+}
